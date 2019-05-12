624d27e1eb1f6cbcaa468751b459b6e2844f9510
Matt <mjohnson@upei.ca>
2019-05-12 07:50:32 -0300

    m12

--- data_wrangling.py
+++ data_wrangling.py
@@ -5,12 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-
 # E4 (wrist) Sampling Frequencies
 fs_dict = {'ACC': 32, 'BVP': 64, 'EDA': 4, 'TEMP': 4, 'label': 700}
-WINDOW_IN_SECONDS = 60
-subject_id = 10
+WINDOW_IN_SECONDS = 15
 label_dict = {'baseline': 1, 'stress': 2, 'amusement': 0}
+feat_names = []
+savePath = 'data'
+
+if not os.path.exists(savePath):
+    os.makedirs(savePath)
+
 
 class SubjectData:
 
@@ -49,61 +53,58 @@ def get_window_stats(data, label=-1):
                 'label': label}
     return features
 
+
 def get_net_accel(data):
-    return (data['ACC_x']**2 + data['ACC_y']**2 + data['ACC_z']**2).apply(lambda x: np.sqrt(x))
+    return (data['ACC_x'] ** 2 + data['ACC_y'] ** 2 + data['ACC_z'] ** 2).apply(lambda x: np.sqrt(x))
 
 
-# Make subject data object for Sx
-subject = SubjectData(main_path='data/WESAD', subject_number=subject_id)
+def compute_features(e4_data_dict, norm_type=None):
+    # Dataframes for each sensor type
+    eda_df = pd.DataFrame(e4_data_dict['EDA'], columns=['EDA'])
+    bvp_df = pd.DataFrame(e4_data_dict['BVP'], columns=['BVP'])
+    acc_df = pd.DataFrame(e4_data_dict['ACC'], columns=['ACC_x', 'ACC_y', 'ACC_z'])
+    temp_df = pd.DataFrame(e4_data_dict['TEMP'], columns=['TEMP'])
+    label_df = pd.DataFrame(subject.labels, columns=['label'])
 
-# Empatica e4 data
-e4_data_dict = subject.get_wrist_data()
+    # Adding indices for combination due to differing sampling frequencies
+    eda_df.index = [(1 / fs_dict['EDA']) * i for i in range(len(eda_df))]
+    bvp_df.index = [(1 / fs_dict['BVP']) * i for i in range(len(bvp_df))]
+    acc_df.index = [(1 / fs_dict['ACC']) * i for i in range(len(acc_df))]
+    temp_df.index = [(1 / fs_dict['TEMP']) * i for i in range(len(temp_df))]
+    label_df.index = [(1 / fs_dict['label']) * i for i in range(len(label_df))]
 
-# Dataframes for each sensor type
-eda_df = pd.DataFrame(e4_data_dict['EDA'], columns=['EDA'])
-bvp_df = pd.DataFrame(e4_data_dict['BVP'], columns=['BVP'])
-acc_df = pd.DataFrame(e4_data_dict['ACC'], columns=['ACC_x', 'ACC_y', 'ACC_z'])
-temp_df = pd.DataFrame(e4_data_dict['TEMP'], columns=['TEMP'])
-label_df = pd.DataFrame(subject.labels, columns=['label'])
+    # Change indices to datetime
+    eda_df.index = pd.to_datetime(eda_df.index, unit='s')
+    bvp_df.index = pd.to_datetime(bvp_df.index, unit='s')
+    temp_df.index = pd.to_datetime(temp_df.index, unit='s')
+    acc_df.index = pd.to_datetime(acc_df.index, unit='s')
+    label_df.index = pd.to_datetime(label_df.index, unit='s')
 
-# Adding indices for combination due to differing sampling frequencies
-eda_df.index = [(1/fs_dict['EDA'])*i for i in range(len(eda_df))]
-bvp_df.index = [(1/fs_dict['BVP'])*i for i in range(len(bvp_df))]
-acc_df.index = [(1/fs_dict['ACC'])*i for i in range(len(acc_df))]
-temp_df.index = [(1/fs_dict['TEMP'])*i for i in range(len(temp_df))]
-label_df.index = [(1/fs_dict['label'])*i for i in range(len(label_df))]
+    # Combined dataframe - not used yet
+    df = eda_df.join(bvp_df, how='outer')
+    df = df.join(temp_df, how='outer')
+    df = df.join(acc_df, how='outer')
+    df = df.join(label_df, how='outer')
+    df['label'] = df['label'].fillna(method='bfill')
 
-# Change indices to datetime
-eda_df.index = pd.to_datetime(eda_df.index, unit='s')
-bvp_df.index = pd.to_datetime(bvp_df.index, unit='s')
-temp_df.index = pd.to_datetime(temp_df.index, unit='s')
-acc_df.index = pd.to_datetime(acc_df.index, unit='s')
-label_df.index = pd.to_datetime(label_df.index, unit='s')
+    if norm_type is 'std':
+        # std norm
+        df = (df - df.mean()) / df.std()
+    elif norm_type is 'minmax':
+        # minmax norm
+        df = (df - df.min()) / (df.max() - df.min())
 
-# Combined dataframe - not used yet
-df = eda_df.join(bvp_df, how='outer')
-df = df.join(temp_df, how='outer')
-df = df.join(acc_df, how='outer')
-df = df.join(label_df, how='outer')
-df['label'] = df['label'].fillna(method='bfill')
-
-# Groupby
-grouped = df.groupby('label')
-baseline = grouped.get_group(1)
-stress = grouped.get_group(2)
-amusement = grouped.get_group(3)
-
-#
-print(f'Available windows for {subject.name}:')
-n_baseline_wdws = int(len(baseline)/(700*WINDOW_IN_SECONDS))
-n_stress_wdws = int(len(stress)/(700*WINDOW_IN_SECONDS))
-n_amusement_wdws = int(len(amusement)/(700*WINDOW_IN_SECONDS))
-print(f'Baseline: {n_baseline_wdws}\nStress: {n_stress_wdws}\nAmusement: {n_amusement_wdws}')
-
-feat_names = []
+    # Groupby
+    grouped = df.groupby('label')
+    baseline = grouped.get_group(1)
+    stress = grouped.get_group(2)
+    amusement = grouped.get_group(3)
+    return grouped, baseline, stress, amusement
 
 
 def get_samples(data, n_windows, label):
+    global feat_names
+
     samples = []
     window_len = 700 * WINDOW_IN_SECONDS
 
@@ -135,16 +136,66 @@ def get_samples(data, n_windows, label):
 
     return pd.concat(samples)
 
-#
-baseline_samples = get_samples(baseline, n_baseline_wdws, 1)
-stress_samples = get_samples(stress, n_stress_wdws, 2)
-amusement_samples = get_samples(amusement, n_amusement_wdws, 0)
 
-all_samples = pd.concat([baseline_samples, stress_samples, amusement_samples])
-all_samples = pd.concat([all_samples.drop('label', axis=1), pd.get_dummies(all_samples['label'])], axis=1)
-all_samples.drop(['ACC_x_mean',    'ACC_x_std',    'ACC_x_min',    'ACC_x_max',
-                 'ACC_y_mean',    'ACC_y_std',    'ACC_y_min',    'ACC_y_max',
-                 'ACC_z_mean',    'ACC_z_std',    'ACC_z_min',    'ACC_z_max',
-                 ], axis=1, inplace=True)
+def make_patient_data(subject_id):
+    global savePath
 
-all_samples.to_csv(f'S{subject_id}_feats.csv')
+    # Make subject data object for Sx
+    subject = SubjectData(main_path='data/WESAD', subject_number=subject_id)
+
+    # Empatica E4 data
+    e4_data_dict = subject.get_wrist_data()
+
+    # norm type
+    norm_type = None
+
+    # The 3 classes we are classifying
+    grouped, baseline, stress, amusement = compute_features(e4_data_dict, norm_type)
+
+    print(f'Available windows for {subject.name}:')
+    n_baseline_wdws = int(len(baseline) / (700 * WINDOW_IN_SECONDS))
+    n_stress_wdws = int(len(stress) / (700 * WINDOW_IN_SECONDS))
+    n_amusement_wdws = int(len(amusement) / (700 * WINDOW_IN_SECONDS))
+    print(f'Baseline: {n_baseline_wdws}\nStress: {n_stress_wdws}\nAmusement: {n_amusement_wdws}\n')
+
+    #
+    baseline_samples = get_samples(baseline, n_baseline_wdws, 1)
+    stress_samples = get_samples(stress, n_stress_wdws, 2)
+    amusement_samples = get_samples(amusement, n_amusement_wdws, 0)
+
+    all_samples = pd.concat([baseline_samples, stress_samples, amusement_samples])
+    all_samples = pd.concat([all_samples.drop('label', axis=1), pd.get_dummies(all_samples['label'])], axis=1)
+    # Selected Features
+    all_samples = all_samples[['EDA_mean', 'EDA_std', 'EDA_min', 'EDA_max',
+                               'BVP_mean', 'BVP_std', 'BVP_min', 'BVP_max',
+                               'TEMP_mean', 'TEMP_std', 'TEMP_min', 'TEMP_max',
+                               'net_acc_mean', 'net_acc_std', 'net_acc_min', 'net_acc_max',
+                               0, 1, 2]]
+    # Save file as csv (for now)
+    all_samples.to_csv(f'{savePath}/S{subject_id}_feats_{norm_type}.csv')
+
+
+def combine_files(subjects):
+    df_list = []
+    for s in subjects:
+        df = pd.read_csv(f'S{s}_feats.csv', index_col=0)
+        df['subject'] = s
+        df_list.append(df)
+
+    df = pd.concat(df_list)
+
+    df['label'] = (df['0'].astype(str) + df['1'].astype(str) + df['2'].astype(str)).apply(lambda x: x.index('1'))
+    df.drop(['0', '1', '2', 'subject'], axis=1, inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+    df.to_csv('may12_feats.csv')
+
+
+if __name__ == '__main__':
+
+    subject_ids = [3, 4, 5, 6, 7, 8, 9, 10]
+
+    for patient in subject_ids:
+        make_patient_data(patient)
+
+    combine_files(subject_ids)
